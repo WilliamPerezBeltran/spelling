5fccb3b94f96481df74c70f7d1e07ecd55e77f65
Daniel Infante Vargas <daninfpj@gmail.com>
2018-08-13 08:58:08 -0500

    Get data from Datamuse API.

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
+require 'net/http'
+
 module Api::V1
   class ActivitiesController < ApplicationController
+    DATAMUSE_API_BASE = 'https://api.datamuse.com/'
+
     def index
-      activity = Activity.new('Get up')
+      data = JSON.parse(
+        Net::HTTP.get(URI("#{DATAMUSE_API_BASE}words?topics=literature"))
+      )
+
+      activity = Activity.new(data.sample['word'])
 
       render json: ::ActivitySerializer.new(activity).serialized_json
     end
